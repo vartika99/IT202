8efb77474e12125d5b0ae31a0a3b25a07a1c67af
Vartika B Patel <vbp42@afsconnect1.njit.edu>
2019-09-19 10:56:26 -0400

    updated initDb file

--- initDb.php
+++ initDb.php
@@ -28,6 +28,13 @@ try{
 
 	print_r($stmt->errorInfo());
 	echo "<br>" . ($r>0?"Insert successful":"Insert failed") . "<br>";
+
+	$select_query = "select * from `TestUsers` where username = :username";
+	$stmt = $db->prepare($select_query);
+	$r = $stmt->execute(array(":username"=> "Billy"));
+	$results = $stmt->fetch(PDO::FETCH_ASSOC);
+	//print_r($stmt->errorInfo());
+	echo "<pre>" . var_export($results, true) . "</pre>";
 }
 catch(Exception $e){
 	echo $e->getMessage();
