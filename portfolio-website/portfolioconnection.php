586447d4b47a5726ee6fe96f97061c3323afee17
Vartika B Patel <vbp42@afsconnect1.njit.edu>
2019-10-31 19:25:16 -0400

    portfolio connection

--- portfolio-website/portfolioconnection.php
+++ portfolio-website/portfolioconnection.php
@@ -14,7 +14,7 @@ try {
    
     $query = "create table if not exists `LoginPage` (
         `username` varchar(20) not null unique,
-        `password` varchar(15) not null,
+        `password` varchar(15) not null
         ) CHARACTER SET utf8 COLLATE utf8_general_ci";
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
