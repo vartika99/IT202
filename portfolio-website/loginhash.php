5cc991ec3f44b0f7af0551499d055fc12ea16f65
Vartika B Patel <vbp42@afsaccess1.njit.edu>
2019-11-26 17:25:37 -0500

    updated login hashing

--- portfolio-website/loginhash.php
+++ portfolio-website/loginhash.php
@@ -7,55 +7,51 @@ session_start();
 
 function verifyLogin() {
         if(isset($_POST['username']) && isset($_POST['password'])) {
-		$login_username = $_POST['username'];
-		$pass = $_POST['password'];
-        	require("config.php");
-		$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
-        	$db = new PDO($conn_string, $username, $password);
-        	$select_query = "select password from `LoginPage` where username=:username";
-        	$stmt = $db->prepare($select_query);
-        	$stmt->bindParam(':username', $login_username);
-        	$stmt->execute();
-		//print_r($stmt->errorInfo());
-            $response = $stmt->fetch(PDO::FETCH_ASSOC);
-            
+                $login_username = $_POST['username'];
+                $pass = $_POST['password'];
+                require("config.php");
+                $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+                $db = new PDO($conn_string, $username, $password);
+                $select_query = "select password from `LoginPage` where username=:username";
+                $stmt = $db->prepare($select_query);
+                $stmt->bindParam(':username', $login_username);
+                $stmt->execute();
+                //print_r($stmt->errorInfo());
+                $response = $stmt->fetch(PDO::FETCH_ASSOC);
+
             if($response && count($response) > 0){
-				//$hash = password_hash($pass, PASSWORD_BCRYPT);
-				if(password_verify($pass, $response['password'])){
-				$_SESSION["user"] = $login_username;
-				header("Location: home.php");
-					echo "Welcome, " . $response["username"];
-					echo "[" . $response["id"] . "]";
-					$login_username = array("id"=> $response['id'],
-								"name"=> $response['username']
-								);
-					$_SESSION['user'] = $login_username;
-					echo var_export($login_username, true);
-					echo var_export($_SESSION, true);
-					
+                                //$hash = password_hash($pass, PASSWORD_BCRYPT);
+                                if(password_verify($pass, $response['password'])){
+                                        echo "Welcome, " . $response['username'];
+                                        echo "[" . $response['username'] . "]";
+                                        $login_username = array("username"=> $response['username']);
+                                        $_SESSION['user'] = $login_username;
+                                        //echo var_export($login_username, true);
+                                        //echo var_export($_SESSION, true);
+                                        header("Location: home.php");
+
                 }
-                
+
                 echo "hello, " . var_export($_POST['username'], true);
             }
-	
-		else {
-        		echo "invalid username/password";
-    		}
-    	}
-}
 
-?> 
+                else {
+                        echo "invalid username/password";
+                }
+        }
+}
+?>
 <!DOCTYPE html>
 <html>
 <center> <body bgcolor="#e3def8">
-	<h2>Welcome to V&V Portfolio!</h2>
+        <h2>Welcome to V&V Portfolio!</h2>
 
-	<header>
+        <header>
     <nav>
-        <a href="https://web.njit.edu/~vbp39/IT-202/IT202/portfolio-website/registration.php">Register</a>
-        <a href="https://web.njit.edu/~vbp42/IT202/portfolio-website/loginhash.php">Login</a> <br> <br>
+        <a href="registration.php">Register</a>
+        <a href="loginhash.php">Login</a> <br> <br>
     </nav>
-	</header>
+        </header>
 
 <style>
 input { border: 1px solid black; }
@@ -72,6 +68,5 @@ password: <br> <input type="password" name="password" required/> <br> <br>
 <?php
     verifyLogin();
 ?>
-
 </body> </center>
 </html>
