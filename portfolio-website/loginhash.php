0427a30805b036a9d834357351569be025872f8b
Vartika B Patel <vbp42@afsaccess1.njit.edu>
2019-11-26 18:19:50 -0500

    updated login hashing

--- portfolio-website/loginhash.php
+++ portfolio-website/loginhash.php
@@ -7,7 +7,6 @@ session_start();
 
 function verifyLogin() {
         if(isset($_POST['username']) && isset($_POST['password'])) {
-<<<<<<< HEAD
 		$login_username = $_POST['username'];
 		$pass = $_POST['password'];
         	require("config.php");
@@ -20,7 +19,7 @@ function verifyLogin() {
 		//print_r($stmt->errorInfo());
 		$response = $stmt->fetch(PDO::FETCH_ASSOC);
             
-            if($response && count($response) > 0){
+            	if($response && count($response) > 0){
 				//$hash = password_hash($pass, PASSWORD_BCRYPT);
 				if(password_verify($pass, $response['password'])){
 					echo "Welcome, " . $response['username'];
@@ -30,32 +29,8 @@ function verifyLogin() {
 					//echo var_export($login_username, true);
 					//echo var_export($_SESSION, true);
 					header("Location: home.php");
-=======
-                $login_username = $_POST['username'];
-                $pass = $_POST['password'];
-                require("config.php");
-                $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
-                $db = new PDO($conn_string, $username, $password);
-                $select_query = "select password from `LoginPage` where username=:username";
-                $stmt = $db->prepare($select_query);
-                $stmt->bindParam(':username', $login_username);
-                $stmt->execute();
-                //print_r($stmt->errorInfo());
-                $response = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            if($response && count($response) > 0){
-                                //$hash = password_hash($pass, PASSWORD_BCRYPT);
-                                if(password_verify($pass, $response['password'])){
-                                        echo "Welcome, " . $response['username'];
-                                        echo "[" . $response['username'] . "]";
-                                        $login_username = array("username"=> $response['username']);
-                                        $_SESSION['user'] = $login_username;
-                                        //echo var_export($login_username, true);
-                                        //echo var_export($_SESSION, true);
-                                        header("Location: home.php");
->>>>>>> 5cc991ec3f44b0f7af0551499d055fc12ea16f65
-
-                }
+                		}
 
                 echo "hello, " . var_export($_POST['username'], true);
             }
