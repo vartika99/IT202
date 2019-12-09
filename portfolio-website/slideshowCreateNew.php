6237bcc8c37938dabe42520f2b0676c601247e7a
Vanshika B Patel <vbp39@afsconnect1.njit.edu>
2019-12-08 19:47:02 -0500

    added slideshow feature to other portfolio

---
+++ portfolio-website/slideshowCreateNew.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<h3> V&V Portfolio </h3>
+<header>
+    <nav> <center>
+        <a href="home.php">Home</a>
+        <a href="createnew.php">Back to Portfolio</a>
+    </nav> </center> <br>
+</header>
+
+<body> <center>
+    <img class="slides" src="lany.jpg" style="width:45%">
+    <img class="slides" src="tree.jpg" style="width:45%">
+
+    <button class="displayLeft" onclick="plusDivs(-1)">&#10094;</button>
+    <button class="displayRight" onclick="plusDivs(+1)">&#10095;</button>
+
+</body> </center>
+
+<script>
+    var slideNum = 1;
+    showDivs(slideNum);
+
+    function plusDivs(n) {
+        showDivs(slideNum += n);
+    }
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("slides");
+        if (n > x.length) {slideNum = 1} 
+        if (n < 1) {slideNum = x.length};
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none"; 
+        }
+        x[slideNum-1].style.display = "block"; 
+    }
+</script>
+
+</html>
